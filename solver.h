b99e291b5dce97465964a02bf858f4fab041ce50
daniel_estrada <daniel.estrada1@udea.edu.co>
2019-03-12 14:25:46 -0500

    mas correcciones

--- solver.h
+++ solver.h
@@ -3,9 +3,12 @@
     - La funcion solver, definida en el archivo "solver.c" que aplica el metodo
     de integracion RK4.
 */
+#ifndef SOLVER_H
+#define SOLVER_H
 
 void solver(double (*f1)(double, double, double, double, double),
             double (*f2)(double, double, double, double, double),
             double (*f3)(double, double, double, double, double),
             double (*f4)(double, double, double, double, double),
             double *t, double *p1, double *p2, double *p3, double *p4, double h);
+#endif
